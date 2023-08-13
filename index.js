a3c709c95d7162cdda91c81ed13266c3b393bf2c
Deepansh <deepanshsrivastav9711@gmail.com>
2023-08-13 18:20:42 +0530

    Added the theme functionality

--- index.js
+++ index.js
@@ -18,7 +18,7 @@ navButtons.forEach((btn) => {
     })
 })
 
-// const root = document.documentElement
+const root = document.documentElement
 
 
 let  main = document.querySelector('.main')
@@ -27,13 +27,85 @@ let themeButton = document.querySelector('.theme-container')
 
 let themeBox = document.querySelector('.themes')
 
+
+
 themeButton.addEventListener('click',()=>{
 
     main.classList.toggle('blur')
     themeBox.classList.toggle('theme-active')
-
-    
+  
     // root.style.setProperty('--bg', '#112e42 ');
     // root.style.setProperty('--main-color', '#ededed ');
 })
 
+
+
+let selectTheme = document.querySelectorAll('.theme-select')
+
+let myImage = document.querySelector('#my-image')
+
+let back2TopButton = document.querySelector('#back-to-top')
+
+
+function toggleImage(value){
+
+    switch (value) {
+        case 0:
+            myImage.src = 'http://127.0.0.1:5501/Icons/me-blue.png'
+            back2TopButton.src = 'http://127.0.0.1:5501/Icons/Back%20to%20top%20icons/blue-arrow.png'
+            break;
+    
+        case 1:
+            myImage.src = 'http://127.0.0.1:5501/Icons/me-purple.png'
+            back2TopButton.src = 'http://127.0.0.1:5501/Icons/Back%20to%20top%20icons/purple-arrow.png'
+            break;
+    
+        case 2:
+            myImage.src = 'http://127.0.0.1:5501/Icons/me-green.png'
+            back2TopButton.src = 'http://127.0.0.1:5501/Icons/Back%20to%20top%20icons/green-arrow.png'
+            break;
+    
+        case 3:
+            myImage.src = 'http://127.0.0.1:5501/Icons/me-red.png'
+            back2TopButton.src = 'http://127.0.0.1:5501/Icons/Back%20to%20top%20icons/red-arrow.png'
+            break;
+    
+
+
+        default:
+            break;
+    }
+
+}
+
+
+
+
+// Blue Button
+selectTheme[0].addEventListener('click',()=>{
+    root.style.setProperty('--bg', '#081b29 ');
+    root.style.setProperty('--main-color', '#00abf0 ');
+    toggleImage(0)
+
+})
+
+// Purple Button
+selectTheme[1].addEventListener('click',()=>{
+    root.style.setProperty('--bg', '#040c18 ');
+    root.style.setProperty('--main-color', '#614bc3 ');
+    toggleImage(1)
+})
+
+// Teal Button
+selectTheme[2].addEventListener('click',()=>{
+    root.style.setProperty('--bg', '#112B3C ');
+    root.style.setProperty('--main-color', '#61b093 ');
+    toggleImage(2)
+})
+
+// Red Button
+selectTheme[3].addEventListener('click',()=>{
+    root.style.setProperty('--bg', '#2C3639 ');
+    root.style.setProperty('--main-color', '#DC0000 ');
+    toggleImage(3)
+})
